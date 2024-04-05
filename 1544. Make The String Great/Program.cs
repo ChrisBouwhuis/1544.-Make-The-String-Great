dd504ae8ea48db5df211542f5b772ddfad442b45
Chris Bouwhuis <chrisb190203@gmail.com>
2024-04-05 10:00:26 +0200

    Add project files.

---
+++ 1544. Make The String Great/Program.cs
@@ -0,0 +1,13 @@
+﻿using solution;
+namespace _1544._Make_The_String_Great
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            var result = sol.MakeGood("acCAbB");
+            System.Console.WriteLine(result);
+        }
+    }
+}
---
+++ solution/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace solution
+{
+    public class Solution
+    {
+        public string MakeGood(string s)
+        {
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] != s[i + 1])
+                {
+                    if (Char.ToLower(s[i]) == Char.ToLower(s[i + 1]))
+                    {
+                        s = s.Remove(i, 2);
+                        return MakeGood(s);
+                    }
+                }
+            }
+            return s;
+        }
+    }
+}
